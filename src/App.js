821ae95cd7c79b659623b329b05a5618a57034b1
shedupte <shedupte@gmail.com>
2020-12-07 22:42:34 -0500

    search bar added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useEffect, useState } from 'react';
 
 function App() {
+
+  const APP_ID='82841968';
+  const APP_KEY='7794f3dc2d70cff758355fa60bb59ff8';
+
+  const exampleReq= `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const [counter, setCounter]=useState(0);
+
+  useEffect(()=>{
+    console.log("effect has been run");
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">
+          submit
+        </button>
+      </form>
+      <h1>
+        <button onClick={()=> setCounter(counter+1)} >{counter}</button>
+      </h1>
     </div>
   );
 }
 
 export default App;
+    
